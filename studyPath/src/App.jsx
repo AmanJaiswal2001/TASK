0d4e8ec2ca385c34430d85d39df7cc2bee730f77
AmanJaiswal2001 <amanjaisawal537@gmail.com>
2024-11-26 21:47:13 +0530

    appjsx main jsx login signin

--- studyPath/src/App.jsx
+++ studyPath/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-
 import Hero from './components/Hero'
+import { Routes,Route } from 'react-router-dom'
+import Signin from './components/Signin'
+import Login from './components/Login'
 import './App.css'
 
 function App() {
@@ -8,7 +10,12 @@ function App() {
 
   return (
     <>
-<Hero/>
+<Routes>
+ <Route path="/" element={<Hero />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+
     </>
   )
 }

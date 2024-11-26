a4ebd0ffce4bb890e15593647834d8fd5235099b
Bibhu-Dutta <156045152+Bibhu-Dutta@users.noreply.github.com>
2024-11-26 17:25:54 +0530

    Header and Footer Section Completed

--- studyPath/src/App.jsx
+++ studyPath/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 
 import Hero from './components/Hero'
 import './App.css'
+import Nav from './components/Nav'
+import Footer from "./components/Footer"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Hero/>
+      {/* <Hero/> */}
+      <Nav />
+      {/* <Footer /> */}
     </>
   )
 }
